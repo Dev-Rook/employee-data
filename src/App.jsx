925e990dca83268b93b34592a6737afcd246b03c
Dev Rook <110010478+Dev-Rook@users.noreply.github.com>
2022-10-09 12:37:20 -0500

    Upload - Setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React, {useState} from 'react';
+import { BrowserRouter, Routes, Route, Switch } from 'react-router-dom';
+import Styles from './App.module.css';
+
+function App() {
+  return (
+    <div className={Styles.App}>
+
+    </div>
+  );
+}
+
+export default App;
